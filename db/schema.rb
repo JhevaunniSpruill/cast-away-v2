1bab7352ab3d833d648a696984927a974df6c55c
Jesse Tovar <jtovar@uncc.edu>
2019-12-05 21:19:24 +0000

    Generate and run User migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_05_185058) do
+ActiveRecord::Schema.define(version: 2019_12_05_211644) do
 
   create_table "polls", force: :cascade do |t|
     t.text "topic"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
