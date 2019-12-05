a1933f2b6641f22fbbed6331b81187e985a27475
Jesse Tovar <jtovar@uncc.edu>
2019-12-05 21:47:28 +0000

    Complete Create and Show for User

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,8 +1,20 @@
 class UserController < ApplicationController
+    def show
+        @user = User.find(params[:id])
+    end
+    
     def new
     end
     
     def create 
-        render plain: params[:user].inspect
+        @user = User.new(user_params)
+        
+        @user.save
+        redirect_to @user
     end
+end
+
+private 
+def user_params
+    params.require(:user).permit(:username, :password)
 end
