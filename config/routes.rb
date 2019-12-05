cfacaee05a90a9fb685f1db7701b7ed38692f635
Jesse Tovar <jtovar@uncc.edu>
2019-12-05 19:52:50 +0000

    Set the home root view

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
     resources :polls
+    root 'home#index'
     
     get 'user/index'
 end
