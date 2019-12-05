213e95d80153a573b1bfb9e5042e7c80f40d6d60
Jhevaunni <jspruil6@uncc.edu>
2019-12-05 19:46:59 +0000

    add application bootstrap and update and edit feautres for polls

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -15,6 +15,31 @@ class PollsController < ApplicationController
     else
       render 'new'
     end
+    
+  def edit
+    @poll = Poll.find_by_id(params[:id])
+  end
+  
+  def update
+  @poll = Poll.find_by_id(params[:id])
+  if @poll.update_attributes(poll_params)
+    flash[:success] = 'Poll was updated!'
+    redirect_to polls_path
+  else
+    render 'edit'
+  end
+  end
+  
+  def destroy
+  @poll = Poll.find_by_id(params[:id])
+  if @poll.destroy
+    flash[:success] = 'Poll was destroyed!'
+  else
+    flash[:warning] = 'Error destroying poll...'
+  end
+  redirect_to polls_path
+  end
+  
   end
 
   private
