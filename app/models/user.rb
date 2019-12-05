940bd425163da6708c352d94adb2cb086e70faed
Jesse Tovar <jtovar@uncc.edu>
2019-12-05 21:56:48 +0000

    Add error messages for New User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-    validates :username, presence: true, length: {minumum: 5}
+    validates :username, presence: true, length: {minimum: 5}
     validates :password, presence: true, length: {minimum: 8}
 end
