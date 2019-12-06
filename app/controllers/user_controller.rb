8f091e6e4b4827bb84fec0fddedb5fa0f2aac9c3
Jesse Tovar <jtovar@uncc.edu>
2019-12-06 17:42:18 +0000

    Updated the User controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,6 @@
 class UserController < ApplicationController
     def show
-        @user = User.find(params[:id])
+       # @user = User.find(params[:id])
     end
     
     def new
@@ -15,6 +15,20 @@ class UserController < ApplicationController
             
         else
             render 'new'
+        end
+    end
+    
+    def edit
+        @user = User.find(params[:id])
+    end
+    
+    def update 
+        @user = User.find(params[:id])
+        
+        if @user.update(user_params)
+            redirect_to @user
+        else
+            render 'edit'
         end
     end
 end
