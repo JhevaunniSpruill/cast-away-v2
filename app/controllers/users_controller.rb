626eef7881d37c9b71a0ce5e7b5953f66030ba97
Jhevaunni <jspruil6@uncc.edu>
2019-12-09 17:14:58 +0000

    fix voting options to show results

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,10 @@ class UsersController < ApplicationController
 
   # GET /users/1
   # GET /users/1.json
-  def show
-  end
+   def show
+    @user = User.find_by_id(params[:id])
+   end
+
 
   # GET /users/new
   def new
--- app/models/poll.rb
+++ app/models/poll.rb
@@ -7,4 +7,13 @@ accepts_nested_attributes_for :vote_options, :reject_if => :all_blank, :allow_de
 
 validates :topic, presence: true
 validates :vote_options, presence: true
+
+def normalized_votes_for(option)
+  votes_summary == 0 ? 0 : (option.votes.count.to_f / votes_summary) * 100
+end
+
+def votes_summary
+  vote_options.inject(0) {|summary, option| summary + option.votes.count}
+end
+
 end
